54243ce1a4ff9c91ad4963f7d0f09aeb2cd66c80
Oleksii83 <zaaytav@gmail.com>
2021-08-11 20:50:33 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const el = React.createElement("div", { a: 5, b: 10 });
+console.log("🚀 ~ file: index.js ~ line 4 ~ el", el);
+
+//===============================================================================
+
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
